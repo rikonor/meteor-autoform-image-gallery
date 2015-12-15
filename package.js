c9d4f99321e5d51fcb4407b4db3a22d75e538e7b
Or Rikon <rikonor@gmail.com>
2015-12-14 20:02:29 -0500

    Lowered fourseven:scss version requirement

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'rikonor:autoform-image-gallery',
-  version: '0.0.1',
+  version: '0.0.2',
   summary: 'Select images in autoform',
   git: 'https://github.com/rikonor/meteor-autoform-image-gallery.git',
   documentation: 'README.md'
@@ -13,7 +13,7 @@ Package.onUse(function(api) {
     'ecmascript',
     'templating',
     'aldeed:autoform@5.8.0',
-    'fourseven:scss@3.4.1'
+    'fourseven:scss@3.2.0'
   ]);
 
   api.addFiles('lib/client/autoform-image-gallery.html', 'client');
