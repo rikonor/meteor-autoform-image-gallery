cdcc1c330b43eb68306cb364db0a7c7cf843913d
Or Rikon <rikonor@gmail.com>
2015-12-22 22:48:32 -0500

    Removed sass dependencies and ran css through autoprefixer

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'rikonor:autoform-image-gallery',
-  version: '0.0.2',
+  version: '0.0.3',
   summary: 'Select images in autoform',
   git: 'https://github.com/rikonor/meteor-autoform-image-gallery.git',
   documentation: 'README.md'
@@ -10,13 +10,11 @@ Package.onUse(function(api) {
   api.versionsFrom('1.2.1');
 
   api.use([
-    'ecmascript',
-    'templating',
     'aldeed:autoform@5.8.0',
-    'fourseven:scss@3.2.0'
+    'templating',
   ]);
 
   api.addFiles('lib/client/autoform-image-gallery.html', 'client');
   api.addFiles('lib/client/autoform-image-gallery.js', 'client');
-  api.addFiles('lib/client/autoform-image-gallery.scss', 'client');
+  api.addFiles('lib/client/autoform-image-gallery.css', 'client');
 });
